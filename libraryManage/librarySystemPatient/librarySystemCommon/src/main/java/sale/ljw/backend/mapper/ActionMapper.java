db63ac955bb7f85c9a82e8cbc6d5fd7588e74b76
Larimari1314 <LJW1556644@gmail.com>
2022-10-17 16:52:07 +0800

    项目搭建 2022-10-17 项目搭建

---
+++ libraryManage/librarySystemPatient/librarySystemCommon/src/main/java/sale/ljw/backend/mapper/ActionMapper.java
@@ -0,0 +1,18 @@
+package sale.ljw.backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import sale.ljw.backend.pojo.Action;
+
+/**
+ * @author 86155
+ * @description 针对表【Action】的数据库操作Mapper
+ * @createDate 2022-10-17 15:29:15
+ * @Entity generator.pojo.Action
+ */
+public interface ActionMapper extends BaseMapper<Action> {
+
+}
+
+
+
+

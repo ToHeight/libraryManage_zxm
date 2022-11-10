333c44b7f5fd4560add97f0fa3af0ed364d3b8d3
cbzxm <1362468712@qq.com>
2022-11-11 00:06:04 +0800

    2022.11.11 v1.0 reader端完成

---
+++ Front-end-project-files/reader/src/view/activation/activationShow.vue
@@ -0,0 +1,19 @@
+<template>
+ <section>
+   <div class="activationWrap">
+     <div class="activationInfo">
+           当前账号正在激活....请稍后.....
+     </div>
+   </div>
+ </section>
+</template>
+
+<script>
+export default {
+  name: "activation"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Front-end-project-files/reader/src/view/myRegister.vue
@@ -0,0 +1,33 @@
+<template>
+<section>
+  <div class="resgisterWrap">
+
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style scoped>
+.resgisterWrap{
+  width: 1200px;
+  /*height: 500px;*/
+  /*border-radius: 20px;*/
+  /*box-shadow: 0 10px 50px rgba(0, 0, 0, 0.3);*/
+  background-image: url("../assets/library4.png");
+}
+</style>
